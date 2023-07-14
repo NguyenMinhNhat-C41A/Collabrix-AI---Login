2d799fce3f929a7e4ee63360234f4eccb8de4213
NguyenMinhNhat-C41A <toilanmnhat@gmail.com>
2023-07-14 15:58:45 -0400

    firebase

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express")
 const app = express()
 
+//MUSTREPLACE
+const users = []
+
+app.use(express.urlencoded({extended: false}))
+
 app.get('/', (req, res) => {
     res.render("index.ejs")
+})
+
+app.get('/login', (req, res) => {
+    res.render("login.ejs")
+})
+
+app.get('/register', (req, res) => {
+    res.render("register.ejs")
 })
 app.listen(3000)
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC9lqrixrzLqNRlbAysTzq0TPWeAtdzVDQ",
+    authDomain: "login-cllbrxai.firebaseapp.com",
+    projectId: "login-cllbrxai",
+    storageBucket: "login-cllbrxai.appspot.com",
+    messagingSenderId: "262959012076",
+    appId: "1:262959012076:web:96537a2019a26690d3edc9",
+    measurementId: "G-TNYYNGPJQ4"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
